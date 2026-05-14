eea9376e05359bce01419cf6f27f166b4cac1d4f
Nikolaos Papadakis (SpinLnx) <npapnet@gmail.com>
2026-05-14 12:11:57 +0300

    test: added code which scans all files in a directory in roder to find a suitbale candindate.

--- examples/cli-single/compare_resample.py
+++ examples/cli-single/compare_resample.py
@@ -17,13 +17,12 @@ from drive_cycle_calculator.obd_file import OBDFile
 # Determine the path to the raw data file (relative to this script's location)
 current_dir = Path(__file__).parent
 root_dir = current_dir.parent.parent
-file_path = root_dir / "raw_data" / "2019-opsimoulis" / "trackLog-2019-Sep-16_10-58-16.xlsx"
-if not file_path.exists():
-    print(f"File not found: {file_path}")
+DATA_PATH = root_dir / "raw_data" / "2019-opsimoulis" 
+
+xlsx_files = list(DATA_PATH.glob("*.xlsx"))
+
+file_path = xlsx_files[2] if xlsx_files else None
     
-#%%
-df_xlsx = pd.read_excel(file_path)
-print(f"Original DataFrame shape: {df_xlsx.shape}")
 #%%
 # Manual mapping approach (most robust across different environments)
 greek_months = {
@@ -32,22 +31,37 @@ greek_months = {
     'Σεπ': 'Sep', 'Οκτ': 'Oct', 'Νοε': 'Nov', 'Δεκ': 'Dec'
 }
 
-    
-s = df_xlsx[" Device Time"]
 
-    
-# 1. Replace all Greek abbreviations in one go using the dictionary
-# regex=True allows it to find the substrings within the full date strings
-s_clean = s.replace(greek_months, regex=True)
+def convert_greek_dates_to_time(s):
+        
+    # 1. Replace all Greek abbreviations in one go using the dictionary
+    # regex=True allows it to find the substrings within the full date strings
+    s_clean = s.replace(greek_months, regex=True)
 
-# 2. Convert the entire Series to datetime
-df_dt = pd.to_datetime(s_clean, format='%d-%b-%Y %H:%M:%S.%f')
+    # 2. Convert the entire Series to datetime
+    df_dt = pd.to_datetime(s_clean, format='%d-%b-%Y %H:%M:%S.%f')
 
-# 3. Extract just the time (as a timedelta to keep ms and performance)
-time_of_day = df_dt - df_dt.dt.normalize()
+    # 3. Extract just the time (as a timedelta to keep ms and performance)
+    time_of_day = df_dt - df_dt.dt.normalize()
+    return time_of_day
 
-print((time_of_day.diff()).describe())
 
+def read_filepath_and_return_time(file_path):
+    df_xlsx = pd.read_excel(file_path)
+    # print(f"Original DataFrame shape: {df_xlsx.shape}")
+    s = df_xlsx[" Device Time"]
+    return convert_greek_dates_to_time(s)   
+
+
+max_times=[print(i, file_path.stem, ",",read_filepath_and_return_time(file_path).diff().max()) for i, file_path in enumerate(xlsx_files) ]
+#%%
+xslx_id = 0 
+df_xlsx = pd.read_excel(xlsx_files[xslx_id ])
+#%%
+s  = df_xlsx[" Device Time"]
+df_xlsx['time']  = convert_greek_dates_to_time(s)
+import matplotlib.pyplot as plt
+plt.hist(df_xlsx.time.dt.total_seconds().diff(), log="y")
 #%%
 print(f"Loading {file_path.name}...")
 obd = OBDFile.from_xlsx(file_path)
